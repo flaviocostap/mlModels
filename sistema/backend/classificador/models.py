e93d8b10871fcd22b5777768c4f671071b65d4d1
Jonnatas <jonatas_lenon@hotmail.com>
2018-09-25 15:50:49 -0300

    backend

--- sistema/backend/classificador/models.py
+++ sistema/backend/classificador/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Feature(models.Model):
-    nome = models.TextField()
-    idade = models.IntegerField()
+    nome = models.TextField(blank=False, null=False,)
+    idade = models.IntegerField(blank=False, null=False,)
     SEX_CHOICE = (
         ('M', 'M'),
         ('F', 'F')
     )
     sexo = models.CharField(choices=SEX_CHOICE, default='M', max_length=2)
-    dado = models.FileField(upload_to='edf') 
+    dado = models.FileField(default=None, blank=False, null=False, upload_to='edf') 
     def __str__(self):
         return self.nome
