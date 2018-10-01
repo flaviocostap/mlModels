f1002daf1f89d8e939d5a6459991e0cbf2ee58e1
Jonnatas <jonatas_lenon@hotmail.com>
2018-10-01 12:10:47 -0300

    melhorando layout

--- sistema/frontend/src/components/cadastro.js
+++ sistema/frontend/src/components/cadastro.js
@@ -9,38 +9,41 @@ class Cadastro extends Component {
   render() {
 
     return (
-      <div className="container-fluid">
-        <form onSubmit={this.props.handleSubmit}>
-          <div className="form-group">
-            <label for="exampleInputNome" className="col-sm-2 col-form-label">Nome</label>
-            <div className="col-sm-10">
+      <div className="col-11 offset-1 container-fluid">
+        <form onSubmit={this.props.handleSubmit} >
+          <div className="col-sm-10 form-group">
+            <label for="exampleInputNome" className="col-form-label">Nome</label>
+            <div >
               <input type="text" className="form-control" id="inputNome" value={this.props.inputNome} onChange={this.props.handleNameChange} placeholder="Nome completo" ></input>
               {/**********   This is where the magic happens     ***********/}
               {this.props.validator.message('inputNome', this.props.inputNome, 'required|alpha')}
 
             </div>
           </div>
-          <div className="form-group">
-            <label for="inputIdade" className="col-sm-2 col-form-label">Idade</label>
-            <div className="col-sm-10">
+          <div className="col-sm-10 form-group">
+            <label for="inputIdade" className=" col-form-label">Idade</label>
+            <div className="">
               <input type="number" className="form-control" value={this.props.inputIdade} onChange={this.props.handleIdadeChange} id="inputIdade" placeholder="idade"></input>
             </div>
           </div>
-          <div className="form-group">
-            <label className="col-sm-10  col-form-label" for="inputSexo">Sexo</label>
+          <div className="col-sm-10 form-group">
+            <label className="col-form-label" for="inputSexo">Sexo</label>
             <select className="col-sm-1 form-control" id="fromControlSelect" value={this.props.selectSexo} onChange={this.props.handleSexoChange}>
               <option>M</option>
               <option>F</option>
             </select>
           </div>
-          <div className="form-group">
+          <div className="col-sm-10 form-group">
             <form>
-              <label className="col-sm-10 col-form-label" for="FormControlFile">Insira o arquivo EMG aqui!</label>
+              <label className="col-md-offset-2" for="FormControlFile">Insira o arquivo EMG aqui!</label>
               <input type="file" className="col-sm-4 form-control-file" id="edfFormControlFile"></input>
             </form>
 
           </div>
-          <button type="submit" class="btn btn-primary">Enviar</button>
+          <div className="col-sm-10 form-group">
+          <button type="submit" class=" btn btn-primary">Enviar</button>
+          </div>
+          
         </form>
       </div>
     )
