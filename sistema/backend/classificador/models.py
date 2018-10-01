3c1805fc433d5add520ea5651196118b24d07019
Jonnatas <jonatas_lenon@hotmail.com>
2018-10-01 15:43:13 -0300

    add backend

--- sistema/backend/classificador/models.py
+++ sistema/backend/classificador/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 import time
-import pickle
+from sklearn.externals import joblib
+import pyedflib
+
+def fft(df, canal):
+    amostras_validas = df[canal][2000:11000]
+    amostras = int(amostras_validas.shape[0]*500/2000)
+    sinal = np.abs(np.fft.fft(amostras_validas))[:amostras]
+    freq = np.linspace(0,500,amostras)
+
+    return (sinal,freq)
+
+def frequecia(df, canal):
+    sinalFFT = fft(df, canal)[0]
+    return sinalFFT
+
+def carregarDataFrame(caminho):
+    edf = pyedflib.EdfReader(caminho)
+    n = edf.signals_in_file
+    sigbufs = np.zeros((n, edf.getNSamples()[0]))
+    for i in np.arange(n):
+         sigbufs[i, :] = edf.readSignal(i)
+    edf._close()
+    del edf
+    data = sigbufs.T
+    
+    return pd.DataFrame(data=data, columns=['ch1', 'ch2', 'ch3', 'ch4'])
+
+def returnCLF(filename, caminho):
+    loaded_model = joblib.load(filename)
+    edf = carregarDataFrame(caminho)
+    sinal = frequecia(edf, 'ch1')
+
+    result = loaded_model.fit(sinal)
+    return result
 
 # Create your models here.
 class Feature(models.Model):
@@ -17,9 +50,8 @@ class Feature(models.Model):
     )
     sexo = models.CharField(choices=SEX_CHOICE, default='M', max_length=2)
     dado = models.FileField(default=None, blank=False, null=False, upload_to=_get_upload_to) 
+    resultSVM = returnCLF('svm.sav', dado)
+    resultRFC = returnCLF('rfc.sav', dado)
+
     def __str__(self):
         return self.nome
-
-class CLF():
-    svmPK = pickle.dumps('svm')
-    rfcPK = pickle.dumps('rfc')
--- sistema/frontend/src/components/navbar.js
+++ sistema/frontend/src/components/navbar.js
@@ -8,7 +8,7 @@ class NavBar extends Component {
                     <div className="bg-dark p-4">
                         <h5 className="text-white h4">Collapsed content</h5>
                         <span className="text-muted">Toggleable via the navbar brand.</span>
-                    </div>z
+                    </div>
                 </div>
                 <nav className="navbar navbar-dark bg-dark">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
