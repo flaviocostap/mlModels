114da918753ee46310b88705cd99a0d003fd7863
Jonnatas <jonatas_lenon@hotmail.com>
2018-10-02 15:27:20 -0300

    classificando

--- sistema/backend/api/settings.py
+++ sistema/backend/api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 MEDIA_ROOT =  os.path.dirname(os.path.abspath(__file__))
-MEDIA_URL = '/df_dados/'
+MEDIA_URL = '/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- sistema/backend/classificador/views.py
+++ sistema/backend/classificador/views.py
@@ -6,6 +6,13 @@ from django.shortcuts import get_object_or_404
 from .models import Feature
 from .serializers import FeatureSerializer
 
+from sklearn.externals import joblib
+import pickle
+import pyedflib
+import pandas as pd
+import scipy as sp
+import numpy as np
+
 class ListAllFeatures(generics.ListCreateAPIView):
     queryset = Feature.objects.all()
     serializer_class = FeatureSerializer
@@ -14,15 +21,45 @@ class DetailFeature(generics.RetrieveDestroyAPIView):
     queryset = Feature.objects.all()
     serializer_class = FeatureSerializer
 
+def carregarDataFrame(caminho):
+    edf = pyedflib.EdfReader(caminho)
+    n = edf.signals_in_file
+    sigbufs = np.zeros((n, edf.getNSamples()[0]))
+    for i in np.arange(n):
+         sigbufs[i, :] = edf.readSignal(i)
+    edf._close()
+    del edf
+    data = sigbufs.T
+    
+    return pd.DataFrame(data=data, columns=['ch1', 'ch2', 'ch3', 'ch4'])
+
+def fft(df, canal):
+    amostras_validas = df[canal][2000:11000]
+    amostras = int(amostras_validas.shape[0]*500/2000)
+    sinal = np.abs(np.fft.fft(amostras_validas))[:amostras]
+    freq = np.linspace(0,500,amostras)
+
+    return (sinal,freq)
+
+def frequecia(df, canal):
+    sinalFFT = fft(df, canal)[0]
+    return sinalFFT
+
 class CLS(APIView):
     def get(self, request, pk):
-        print(pk)
-        feature = get_object_or_404(Feature, pk=pk)
-        data = FeatureSerializer(feature).data
-        caminho = data['dado']
+        feature = Feature.objects.get(id=pk)
+        caminho = feature.dado.path
+        df = carregarDataFrame(caminho)
+        sinal = frequecia(df, 'ch1')
 
-        entry = Feature.objects.filter(pk=pk).update(result='10')
-        
+        # # Load from file
+        joblib_file = "rfc.pkl"
+        path = '/home/jonnatas/git/mlModels/sistema/backend/api/edf/'+joblib_file
+        loaded_model = joblib.load(path)
+
+        pred = loaded_model.predict([sinal])
+
+        entry = Feature.objects.filter(pk=pk).update(result=pred)
         feature = get_object_or_404(Feature, pk=pk)
         data = FeatureSerializer(feature).data
         
