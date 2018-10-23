cd5cd680cfd34fff4e0a075d3a5af03a16168776
Jonnatas <jonatas_lenon@hotmail.com>
2018-10-23 15:04:59 -0300

    add material ui

--- sistema/frontend/src/components/home/listItens.js
+++ sistema/frontend/src/components/home/listItens.js
@@ -1,5 +1,74 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import Button from '@material-ui/core/Button';
+import MUIDataTable from "mui-datatables";
+
+
+const columns = [
+    {
+        name: "Nº",
+        options: {
+            filter: true,
+            sort: true,
+        }
+    },
+    {
+        name: "Nome",
+        options: {
+            filter: true,
+            sort: false,
+        }
+    },
+    {
+        name: "Idade",
+        options: {
+            filter: true,
+            sort: false,
+        }
+    },
+    {
+        name: "Sexo",
+        options: {
+            filter: true,
+            sort: false,
+        }
+    },
+    {
+        name: "Resultado sobre a DP",
+        options: {
+            filter: true,
+            viewColumns: false,
+            sort: false,
+        }
+    },
+    {
+        name: "Opções",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    }, {
+        name: "Id",
+        options: {
+            filter: false,
+            sort: false,
+            display: false,
+        }
+    },
+];
+
+const data = [
+    ["Joe James", "Test Corp", "Yonkers", "Yonkers", "NY", <Button color="secondary">teste</Button>, "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT", "Yonkers", <Button color="secondary">teste</Button>, "NY"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL", "NY", <Button color="secondary">teste</Button>, "Yonkers",],
+    ["James Houston", "Test Corp", "Dallas", "TX", "NY", <Button color="secondary">teste</Button>, "NY"],
+];
+
+const options = {
+    filterType: 'checkbox',
+};
+
+
 
 class ListItens extends Component {
     constructor(props) {
@@ -29,7 +98,14 @@ class ListItens extends Component {
             Alta probabilidade
       </div>;
     }
+
     render() {
+
+        let dataFeature = this.props.features.map((currElement, index) => {
+            if (currElement.arquivar === this.props.exibirArquivados)
+                return [index + 1, currElement.nome, currElement.idade, currElement.sexo, currElement.result, currElement.id]
+        })
+        dataFeature = dataFeature.filter(a=>{return a})
         let features
         if (this.props.pesquisa === null) {
             features = this.props.features
@@ -42,8 +118,16 @@ class ListItens extends Component {
         }
         return (
             <div class="container">
+
+                <MUIDataTable
+                    title={"Employee List"}
+                    data={dataFeature}
+                    columns={columns}
+                    options={options}
+                />
                 <div class="table-responsive">
-                    <table class="table">
+                    {/* <table id="example" class="display"> */}
+                    <table id="listaDeUsuarios" class="table">
                         <caption>Usuários</caption>
                         <thead>
                             <tr>
@@ -65,7 +149,7 @@ class ListItens extends Component {
                                                     <td>{item.nome}</td>
                                                     <td>{item.idade}</td>
                                                     <td>{item.sexo}</td>
-                                                    <td>{item.result === null ? <button type="button" class="btn btn-outline-info" onClick={() => this.props.avaliarPatient(item)}>Avaliar</button> : this.result(item.result)}</td> 
+                                                    <td>{item.result === null ? <button type="button" class="btn btn-outline-info" onClick={() => this.props.avaliarPatient(item)}>Avaliar</button> : this.result(item.result)}</td>
                                                     <td>
                                                         <div class="btn-group">
                                                             <button type="button" class="btn btn-outline-warning" onClick={() => this.props.updatePatient(item)} data-toggle="modal" data-target="#editModal">Editar</button>
@@ -78,6 +162,15 @@ class ListItens extends Component {
                                         }
                                     })}
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <th scope="col">Nº</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Idade</th>
+                                <th scope="col">Sexo</th>
+                                <th scope="col">Resultado sobre a DP</th>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
